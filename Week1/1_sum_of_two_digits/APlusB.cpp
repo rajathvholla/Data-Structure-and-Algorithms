bad06ab92851496bd67c1bdf37655db9440c3b45
unknown <rajath.v.holla.phone@gmail.com>
2019-04-04 23:25:17 -0700

    Testing commits

--- Week1/1_sum_of_two_digits/APlusB.cpp
+++ Week1/1_sum_of_two_digits/APlusB.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 
-long long sum_of_two_digits(long long first_digit, long long  second_digit) {
+long long sum_of_two_digits(long long first_digit, long long second_digit)
+{
     return first_digit + second_digit;
 }
 
-int main() {
-    long long  a = 0;
-    long long  b = 0;
+int main()
+{
+    // Change everything to lonng long
+    long long a = 0;
+    long long b = 0;
     std::cin >> a;
     std::cin >> b;
     std::cout << sum_of_two_digits(a, b);
     return 0;
 }
-
