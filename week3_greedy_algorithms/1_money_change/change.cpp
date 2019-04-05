5aefefba39fe239ca7e9b6bb7195f1e244d84583
Rajath Vishwanath Holla <rajathv@nvidia.com>
2019-04-05 15:20:54 -0700

    Working change program.

--- week3_greedy_algorithms/1_money_change/change.cpp
+++ week3_greedy_algorithms/1_money_change/change.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 
-int get_change(int m) {
-  //write your code here
-  return n;
+int get_change(int m)
+{
+  int count = 0;
+  count += m / 10;
+  m = m % 10;
+  count += m / 5;
+  m = m % 5;
+  count += m;
+
+  return count;
 }
 
-int main() {
+int main()
+{
   int m;
+
   std::cin >> m;
   std::cout << get_change(m) << '\n';
 }
