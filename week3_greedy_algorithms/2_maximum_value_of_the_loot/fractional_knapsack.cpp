11e1b13e1d37429cde1ae8f4a951db287e7e4904
Rajath Vishwanath Holla <rajathv@nvidia.com>
2019-04-05 17:07:10 -0700

    To be completed.

--- week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
+++ week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
@@ -1,25 +1,53 @@
 #include <iostream>
 #include <vector>
+#include <tuple>
+#include <algorithm>
+using namespace std;
 
-using std::vector;
-
-double get_optimal_value(int capacity, vector<int> weights, vector<int> values) {
+double get_optimal_value(int capacity, vector<int> weights, vector<int> values)
+{
   double value = 0.0;
 
-  // write your code here
+  vector<std::tuple<double, int, int>> objects;
+
+  for (auto weights_it = weights.begin(), values_it = values.begin(); weights_it != weights.end(); weights_it++, values_it++)
+  {
+    objects.push_back(std::make_tuple((*values_it) / (*weights_it), *values_it, *weights_it));
+  }
+  sort(objects.begin(), objects.end(), [](std::tuple<double, int, int> a, std::tuple<double, int, int> b) { return get<0>(a) > get<0>(b); });
+
+  int idx = 0;
+  while (capacity != 0)
+  {
+    int idx_val_per_weight;
+    int idx_value;
+    int idx_weight;
+    std::tie(idx_val_per_weight, idx_value, idx_weight) = objects[idx];
+    int quantity = (capacity > idx_weight) ? idx_weight : capacity;
+
+    value += (quantity * idx_val_per_weight);
+    capacity -= quantity;
+    idx++;
+  }
 
   return value;
 }
 
-int main() {
+int main()
+{
   int n;
   int capacity;
-  std::cin >> n >> capacity;
-  vector<int> values(n);
-  vector<int> weights(n);
-  for (int i = 0; i < n; i++) {
+  //std::cin >> n >> capacity;
+  n = 3;
+  capacity = 50;
+  vector<int> values = {60, 100, 120};
+  vector<int> weights = {20, 50, 30};
+  /*for (int i = 0; i < n; i++)
+  {
     std::cin >> values[i] >> weights[i];
   }
+
+  */
 
   double optimal_value = get_optimal_value(capacity, weights, values);
 
