3e5cc404095d104f6fb87505aa9963ad8ecce28c
Rajath Vishwanath Holla <rajathv@nvidia.com>
2019-04-30 17:18:54 -0700

    4/30/2019 commit 1

--- week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.cpp
+++ week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.cpp
@@ -3,23 +3,53 @@
 
 using std::vector;
 
-int optimal_weight(int W, const vector<int> &w) {
+int optimal_weight(int W, const vector<int> &w)
+{
   //write your code here
   int current_weight = 0;
-  for (size_t i = 0; i < w.size(); ++i) {
-    if (current_weight + w[i] <= W) {
+  for (size_t i = 0; i < w.size(); ++i)
+  {
+    if (current_weight + w[i] <= W)
+    {
       current_weight += w[i];
     }
   }
   return current_weight;
 }
 
-int main() {
+int frac_knap(int W, const vector<int> &weights)
+{
+  int mat[weights.size() + 1][W + 1];
+  for (int i = 0; i <= weights.size(); i++)
+    mat[i][0] = 0;
+  for (int i = 0; i <= W; i++)
+    mat[0][i] = 0;
+
+  int running_weight = 0;
+  for (int i = 1; i <= weights.size(); i++)
+  {
+    for (int w = 1; w <= W; w++)
+    {
+      mat[w][i] = mat[w][i - 1];
+      if (weights[i] <= w)
+      {
+        int temp = mat[weights[i] - w][i - 1] + weights[i];
+        if (mat[w][i] < temp)
+          mat[w][i] = temp;
+      }
+    }
+    return mat[W][weights.size()];
+  }
+}
+
+int main()
+{
   int n, W;
   std::cin >> W >> n;
   vector<int> w(n);
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < n; i++)
+  {
     std::cin >> w[i];
   }
-  std::cout << optimal_weight(W, w) << '\n';
+  std::cout << frac_knap(W, w) << '\n';
 }
